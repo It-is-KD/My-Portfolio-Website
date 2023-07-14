fde73f50134d571a91bc59d0f12f6aefa92950fd
Khush Desai <114131222+It-is-KD@users.noreply.github.com>
2023-07-14 11:38:06 +0530

    Update main.js

--- Portfolio Website/JS/main.js
+++ Portfolio Website/JS/main.js
@@ -145,3 +145,19 @@ let swiper = new Swiper(".project_container", {
     },
     
   });
+
+/*--------------------------------------------------------------------------------SMTP*/
+  function sendEmail() {
+    Email.send({
+        SecureToken: "",
+        Host : "",
+        Username : "",
+        Password : "",
+        To : '',
+        From : '',
+        Subject : "FormSubmission: " + document.getElementById("subject").value,
+        Body : "Name: " + document.getElementById("name").value + "<br>" + "Email: " + document.getElementById("email").value + "<br><br>" + document.getElementById("text").value
+    }).then(
+      message => alert("Message Sent Successfully 🫠")
+    );
+  }
